b01437e392b004de60029ac6199b6fd69b76709b
Oner Ozbey <onerozbey@gmail.com>
2026-01-06 13:45:40 +0300

    Reduce native swipe stack size by 10%

--- src/components/SwipeStack.tsx
+++ src/components/SwipeStack.tsx
@@ -149,7 +149,7 @@ const SwipeStack: React.FC<SwipeStackProps> = ({
   }
 
   return (
-    <div className={`relative w-full ${className}`} style={{ height: 'calc(100svh - 280px)', minHeight: '400px', maxHeight: '700px' }}>
+    <div className={`swipe-stack relative w-full ${className}`} style={{ height: 'calc(100svh - 280px)', minHeight: '400px', maxHeight: '700px' }}>
       {/* Card Stack */}
       <div className="w-full h-full relative">
         <AnimatePresence>
